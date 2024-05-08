b4bc0b5272c984a6a5657de60d3b29e524f0ea61
Igor <igorpacholski@gmail.com>
2024-05-08 11:43:53 +0200

    added checkout

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -38,26 +38,30 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           <DeleteConfirmation eventId={event._id} />
         </div>
       )}
-      <Link
-        href={`/events/${event._id}`}
-        className="flex min-h-[280px] flex-col gap-3 p-5 md:gap-4"
-      >
-        {!hidePrice && (
-          <div className="flex gap-2">
-            <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
-              {event.isFree ? "FREE" : `$${event.price}`}
-            </span>
-            <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
-              {event.category.name}
-            </p>
-          </div>
-        )}
-        <p className="p-medium-16 md:p-medium-18 text-grey-500">
-          {formatDateTime(event.startDateTime).dateTime}
-        </p>
-        <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
-          {event.title}
-        </p>
+      <div className="flex min-h-[280px] flex-col gap-3 p-5 md:gap-4">
+        <Link
+          href={`/events/${event._id}`}
+          className="flex flex-1 flex-col gap-3"
+        >
+          {!hidePrice && (
+            <div className="flex gap-2">
+              <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
+                {event.isFree ? "FREE" : `$${event.price}`}
+              </span>
+              <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
+                {event.category.name}
+              </p>
+            </div>
+          )}
+          <p className="p-medium-16 md:p-medium-18 text-grey-500">
+            {formatDateTime(event.startDateTime).dateTime}
+          </p>
+
+          <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+            {event.title}
+          </p>
+        </Link>
+
         <div className="flex-between w-full">
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
             {event.organizer.firstName} {event.organizer.lastName}
@@ -75,7 +79,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
             </Link>
           )}
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
---
+++ components/shared/Checkout.tsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import { Button } from "../ui/button";
+import { IEvent } from "@/lib/mongodb/database/models/event.model";
+import { loadStripe } from "@stripe/stripe-js";
+import { checkoutOrder } from "@/lib/mongodb/actions/order.actions";
+
+loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
+type CheckoutProps = {
+  event: IEvent;
+  userId: string;
+};
+
+const Checkout = ({ event, userId }: CheckoutProps) => {
+  const onCheckout = async () => {
+    const order = {
+      eventTitle: event.title,
+      eventId: event._id,
+      price: event.price,
+      isFree: event.isFree,
+      buyerId: userId,
+    };
+
+    await checkoutOrder(order);
+  };
+
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
+    }
+  }, []);
+
+  return (
+    <form action={onCheckout} method="POST">
+      <Button type="submit" role="link" size="lg" className="button sm:w-fit">
+        {event.isFree ? "Get Ticket" : "Buy Ticket"}
+      </Button>
+    </form>
+  );
+};
+
+export default Checkout;
